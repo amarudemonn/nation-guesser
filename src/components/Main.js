ca1c9e7dbf053a7dfdf804ed6054f8ea030a4464
amarudemonn <amarudemonn@gmail.com>
2021-02-25 08:13:34 +0300

    White space bug fix

--- src/components/Main.js
+++ src/components/Main.js
@@ -14,7 +14,7 @@ class Main extends React.Component {
   }
 
   nameHandler = (e) => {
-    this.setState({ name: e.target.value });
+    this.setState({ name: e.target.value.trim() });
   };
 
   onSubmitHandler = (e) => {
